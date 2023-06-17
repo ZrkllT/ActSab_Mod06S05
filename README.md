81bc7a20ef6b7645a0d16c95654cbb9c3e24e3a6
ZrkllT <zero.kullt@gmail.com>
2023-06-17 13:57:10 -0400

    sab jun-17 (4)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ActSab_Mod06S05
+
+1- Crear repositorio remoto con Readme y gitignore incluido.
+2- Clonar proyecto en su entorno local.
+3- intalar la extensión faker. https://fakerjs.dev/guide/
+4- Dentro de una función crear un ciclo que genere 10 personas aleatorias utilizando Faker. Se debe calcular la edad de cada persona. (Id, Nombre, Apellido, Fecha Nacimiento, Ciudad)
+5- Crear una función que muestre cuantas personas viven en cada ciudad.
+6- Crear una función que genere un numero aleatorio de 8 digitos y utilizando la dependencia rut.js obtener el dígito verificador. Se debe incluir esta función en la creación de los objetos de personas del requerimiento 4.
--- src/personas_fake.js
+++ src/personas_fake.js
@@ -1,15 +1,3 @@
-/*
-** 1- Crear repositorio remoto con Readme y gitignore incluido.
-** 2- Clonar proyecto en su entorno local.
-** 3- intalar la extensión faker. https://fakerjs.dev/guide/
-**4- Dentro de una función crear un ciclo que genere 10 personas aleatorias utilizando Faker.
-    Se debe calcular la edad de cada persona. (Id, Nombre, Apellido, Fecha Nacimiento, Ciudad)
-5- Crear una función que muestre cuantas personas viven en cada ciudad.
-**6- Crear una función que genere un numero aleatorio de 8 digitos y utilizando la dependencia rut.js 
-    obtener el dígito verificador. Se debe incluir esta función en la creación de los objetos de 
-    personas del requerimiento 4.
-*/
-
 import { faker, es, fakerES_MX } from '@faker-js/faker'
 import { getCheckDigit, format } from 'rut.js'
 import moment from 'moment'
