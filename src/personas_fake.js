3fab87c85e3e1ff59809affdbf1bff2ea391b5d7
ZrkllT <zero.kullt@gmail.com>
2023-06-17 13:55:53 -0400

    sab jun-17 (3)

--- src/personas_fake.js
+++ src/personas_fake.js
@@ -2,10 +2,10 @@
 ** 1- Crear repositorio remoto con Readme y gitignore incluido.
 ** 2- Clonar proyecto en su entorno local.
 ** 3- intalar la extensión faker. https://fakerjs.dev/guide/
-4- Dentro de una función crear un ciclo que genere 10 personas aleatorias utilizando Faker.
+**4- Dentro de una función crear un ciclo que genere 10 personas aleatorias utilizando Faker.
     Se debe calcular la edad de cada persona. (Id, Nombre, Apellido, Fecha Nacimiento, Ciudad)
 5- Crear una función que muestre cuantas personas viven en cada ciudad.
-6- Crear una función que genere un numero aleatorio de 8 digitos y utilizando la dependencia rut.js 
+**6- Crear una función que genere un numero aleatorio de 8 digitos y utilizando la dependencia rut.js 
     obtener el dígito verificador. Se debe incluir esta función en la creación de los objetos de 
     personas del requerimiento 4.
 */
@@ -25,15 +25,14 @@ function genRut(){
     var min = 1_000_000
     var max = 29_999_999
 
-    return (Math.floor(Math.random() * (max - min + 1) + min)).toString()
+    return genDV((Math.floor(Math.random() * (max - min + 1) + min)).toString())
 }
 function genDV(rut){
-    var dv = getCheckDigit(rut)
-    return dv
+    return rut + getCheckDigit(rut)
 }
 
 function generarFaker(){
-    var rutGen = genRut() +'-'+ genDV(genRut())
+    var rutGen = genRut()
     var fecha_nacimiento = faker.date.birthdate()
     var fecha_hoy = moment()
     var edad = moment(fecha_hoy).diff(fecha_nacimiento,'years')
